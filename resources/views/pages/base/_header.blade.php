4633030a3d840c810ee0530f2051172462b51520
Andreas Suryadi <andreas.suryadi02@gmail.com>
2021-05-17 21:43:34 +0700

    Update for home page banner

--- resources/views/pages/base/_header.blade.php
+++ resources/views/pages/base/_header.blade.php
@@ -1,4 +1,5 @@
 <head>
     <title>Saung Bagja</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
