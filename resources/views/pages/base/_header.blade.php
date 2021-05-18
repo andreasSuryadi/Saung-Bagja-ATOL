783857b846bc45f1f48b285299dc0a8bca7ba6f6
Andreas Suryadi <andreas.suryadi02@gmail.com>
2021-05-18 21:27:42 +0700

    Update For About And Features Section

--- resources/views/pages/base/_header.blade.php
+++ resources/views/pages/base/_header.blade.php
@@ -1,5 +1,8 @@
 <head>
     <title>Saung Bagja</title>
+
+    <link rel="shortcut icon" href="{{ asset('images/icon/favicon.ico') }}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="/resources/css/icon-font.css">
 </head>
