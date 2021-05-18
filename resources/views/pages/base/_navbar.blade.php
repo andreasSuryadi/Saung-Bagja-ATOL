8abd2f23b12cbfbd39659bf65ad202aabe11a9d9
Andreas Suryadi <andreas.suryadi02@gmail.com>
2021-05-18 21:46:16 +0700

    Update navbar

--- resources/views/pages/base/_navbar.blade.php
+++ resources/views/pages/base/_navbar.blade.php
@@ -1,10 +1,11 @@
 <nav>
-    <ul class="flex-center">
+    <ul class="navbar">
         <li><a href="#" class="home"><img src="{{ asset('images/content/logo.png') }}"></a></li>
-        <li><a class="on">ABOUT US</a></li>
-        <li><a href="#">MENU</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">CONTACT US</a></li>
-        <li><a href="#">CAREER</a></li>
+        <li><a class="blank"></a></li>
+        <li><a href="/about-us" class="on">ABOUT US</a></li>
+        <li><a href="#" class="menu">MENU</a></li>
+        <li><a href="#" class="menu">NEWS</a></li>
+        <li><a href="#" class="menu">CONTACT US</a></li>
+        <li><a href="#" class="menu">CAREER</a></li>
     </ul>
 </nav>
